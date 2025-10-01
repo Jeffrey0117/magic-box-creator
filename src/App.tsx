db2940a3452b854f443b00331f317801d5591688
Jeffrey0117 <jeffby8@gmail.com>
2025-10-02 03:09:51 +0800

    fix: V7.3 修正頁面切換後滾動位置問題（ScrollToTop 元件）

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
+import { useEffect } from "react";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import Box from "./pages/Box";
 import Login from "./pages/Login";
 import Creator from "./pages/Creator";
@@ -12,12 +13,23 @@ import Help from "./pages/Help";
 
 const queryClient = new QueryClient();
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
       <Toaster />
       <Sonner />
       <BrowserRouter>
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<Box />} />
           <Route path="/login" element={<Login />} />
