c5853df5d75626eb5813cafb8afd6d0b8bbb06f5
Jeffrey0117 <jeffby8@gmail.com>
2025-10-02 13:33:41 +0800

    fix: 修正首頁路由（應顯示 Index 而非 Box）

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import Index from "./pages/Index";
 import Box from "./pages/Box";
 import Login from "./pages/Login";
 import Creator from "./pages/Creator";
@@ -31,7 +32,7 @@ const App = () => (
       <BrowserRouter>
         <ScrollToTop />
         <Routes>
-          <Route path="/" element={<Box />} />
+          <Route path="/" element={<Index />} />
           <Route path="/login" element={<Login />} />
           <Route path="/creator" element={<Creator />} />
           <Route path="/privacy" element={<Privacy />} />
